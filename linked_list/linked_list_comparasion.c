b2a235eff58bab868fe7b28f9ab6e1b1e4f5e82d
Nailton Reis <nazin.dev@gmail.com>
2026-01-12 10:19:44 -0300

    Linked List Version 2

--- linked_list/linked_list_comparasion.c
+++ linked_list/linked_list_comparasion.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
+
+// representação do TAD lista duplamente encadeada
 typedef struct node {
     int *value;
     struct node *next;
@@ -13,16 +16,22 @@ typedef struct list {
     size_t length;
 } list_t;
 
-node_t* create_node(int *value);
-void node_print(node_t *self);
 
-list_t* list_create();
-void list_print(list_t *self);
-void list_push(list_t *self, int *value);
+node_t* node_create(int* value); // ok
+void node_print(node_t* self); // ok
+void node_delete(node_t *self); //
+
+list_t* list_create(); // ok
+void list_print(list_t *self); // ok
+size_t list_length(list_t *self); 
+void list_delete(list_t *self); 
+void list_push(list_t *self, int* value); //
+int* list_pop(list_t *self); //
 
 
-node_t* create_node(int *value) {
-    node_t *new_node = (node_t*) malloc(sizeof(node_t*));
+
+node_t* node_create(int* value) {
+    node_t *new_node = (node_t*) malloc(sizeof(node_t));
 
     if (new_node == NULL) {
         perror("Memory Error:");
@@ -32,12 +41,18 @@ node_t* create_node(int *value) {
     new_node->next = NULL;
     new_node->prev = NULL;
     new_node->value = value;
-
     return new_node;
 }
 
+void node_print(node_t *self) {
+    printf("Node{\n\tNext: %p\n\tPrev: %p\n\tValue: ", self->next, self->prev);
+    printf("%d", *(self->value));
+    printf("\n}\n");
+}
+
+
 list_t* list_create() {
-    list_t *new_list = (list_t*)  malloc(sizeof(list_t));
+    list_t *new_list =  (list_t*) malloc(sizeof(list_t));
 
     if (new_list == NULL) {
         perror("Memory Error:");
@@ -47,13 +62,13 @@ list_t* list_create() {
     new_list->head = NULL;
     new_list->tail = NULL;
     new_list->length = 0;
+    return new_list;
 }
 
 void list_print(list_t *self) {
     printf("List{\n\tLength: %zu\n\tData: [", self->length);
     node_t *current = self->head;
-    while (current != NULL)
-    {
+    while (current != NULL) {
         printf("%d", *(current->value));
         if (current->next != NULL) {
             printf(" -> ");
@@ -64,36 +79,36 @@ void list_print(list_t *self) {
     printf("\n}\n");
 }
 
+
 void list_push(list_t *self, int *value) {
-    node_t *new_node = create_node(value);
+    node_t *new_node = node_create(value); 
 
     if (self->length == 0) {
         self->head = new_node;
         self->tail = new_node;
-        self->length += 1;
-    } else if (self->length > 0) {
+    } 
+    
+    else if (self->length > 0) {
         self->tail->next = new_node;
         new_node->prev = self->tail;
         self->tail = new_node;
-        self->length += 1;
     }
 
-
-}
-
-void node_print(node_t *self) {
-    printf("Node{\n\tNext: %p\n\tPrev: %p\n\tValue: %d", self->next, self->prev, *(self->value));
-    printf("\n}\n");
+    self->length += 1;
 }
 
 
 int main() {
-    int value = 16;
-    list_t *linked_list = list_create();
-    node_t *no1 = create_node(&value);
-    node_print(no1);
+    int value, value2;
+    value = 16;
+    value2 = 5;
 
+    list_t *linked_list = list_create();
     list_push(linked_list, &value);
+    list_push(linked_list, &value2);
     list_print(linked_list);
+    
+
+
     return 0;
 }
